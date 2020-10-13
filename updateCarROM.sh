d6d6844af9f18af4caffe540b6fc106fa903a0b4
xiaoqiang <xiaoqiang@bwbot.org>
2020-10-13 21:46:47 +0800

    try to backport  16.04

--- updateCarROM.sh
+++ updateCarROM.sh
@@ -15,5 +15,5 @@ echo -e -n "\033[0m"
 sudo service startup stop
 echo -e "\033[34m 开始更新小强底盘固件..."
 echo -e -n "\033[0m"
-stm32loader -p /dev/ttyUSB001 -e -w -v mini.bin
+./stm32loader.py -p /dev/ttyUSB001 -e -w -v mini.bin
 fi
